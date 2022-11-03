c78fb2bc35c1413f7b5614184a934f3e00a622de
adysonalves <adyson6@gmail.com>
2022-11-03 13:45:42 -0300

    404

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(express.urlencoded({extended: false}));
 const principal = require('./routes/principal');
 const painel = require('./routes/painel');
 app.use((req,res,next) => {
-    res.status(404).redirect('/');
+    res.redirect('/');
 });
 
 
